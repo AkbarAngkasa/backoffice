ae5b2f7a25a968e8c73b2c05d0b06b92f2e54b09
unknown <akbar@emkop.co.id>
2023-04-25 10:41:11 +0800

    modified

--- src/components/blocks/SidenavBlock.jsx
+++ src/components/blocks/SidenavBlock.jsx
@@ -12,17 +12,9 @@ export default function SidenavBlock() {
 
     return (
         <>
-            {/* Sidenav Open Btn Mobile */}
-            <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="absolute top-0 left-0 inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-                <span className="sr-only">Open sidebar</span>
-                <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-                </svg>
-            </button>
-
             {/* Content */}
-            <aside id="default-sidebar" className="flex flex-row fixed top-0 left-0 z-40 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidenav">
-                <div className="overflow-y-auto py-5 px-3 w-[70%] sm:w-64 h-full bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+            <aside id="default-sidebar" className="flex flex-row fixed top-0 left-0 z-40 w-full h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidenav">
+                <div className="overflow-y-auto py-5 px-3 w-[70%] sm:w-[20%] h-full bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700">
                     <ul className="space-y-2">
                         <li>
                             <img src={logo} alt={logo} />
@@ -83,7 +75,7 @@ export default function SidenavBlock() {
                     </ul>
                 </div>
                 {/* Sidenav Close Btn Mobile */}
-                <div className="bg-transparent w-[30%] sm:hidden" data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar"></div>
+                <div className="bg-transparent w-[30%] sm:w-[80%] sm:hidden" data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar"></div>
             </aside>
         </>
     )
