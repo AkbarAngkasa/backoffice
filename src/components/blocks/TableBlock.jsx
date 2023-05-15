168c1f33d2559f94b47c5c332801095de0853c71
unknown <akbar@emkop.co.id>
2023-05-15 13:24:00 +0800

    modified

--- src/components/blocks/TableBlock.jsx
+++ src/components/blocks/TableBlock.jsx
@@ -274,9 +274,9 @@ export default function TableBlock() {
         return (
             <>
                 {product.user_status === "active" ? 
-                    <span class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">Active</span>
+                    <span className="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">Active</span>
                     :
-                    <span class="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-red-900 dark:text-red-300">Blocked</span>
+                    <span className="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-red-900 dark:text-red-300">Blocked</span>
                 }
             </>
         )
