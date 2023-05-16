e32f56d60ef324dcc68301a33e3a265f1d901318
unknown <akbar@emkop.co.id>
2023-05-16 14:42:12 +0800

    modified useCheckAccessToken.js

--- src/costumHooks/useCheckAccessToken.js
+++ src/costumHooks/useCheckAccessToken.js
@@ -1,28 +1,29 @@
 import { useEffect, useMemo, useState } from 'react';
 import { useNavigate } from 'react-router';
 import Cookies from 'universal-cookie';
+import useGetFullPath from './useGetFullPath';
 
 export default function useCheckAccessToken() {
-     // === Hooks ===
-     const navigate = useNavigate();
-     const cookies = useMemo(() => new Cookies(), []);
+    // === Hooks ===
+    const navigate = useNavigate();
+    const cookies = useMemo(() => new Cookies(), []);
+    const currentPath = useGetFullPath();
 
     const [ userAccessToken, setUserAccessToken ] = useState(null);
 
     useEffect(() => {
         // #. Check is user have an active accessToken?
         let accessToken =  cookies.get('accessToken');
-        console.log(accessToken)
         if(accessToken === undefined){
             // User Is Not Logged In.
             setUserAccessToken(undefined);
             navigate("/login");
         } else {
-            // User Already Logged In.
+            // User Already Logged In, redirect back to its current path.
             setUserAccessToken(accessToken);
-            navigate("/dashboard");
+            navigate(currentPath);
         }
-    }, [userAccessToken, cookies, navigate]);
+    }, [userAccessToken, cookies, navigate, currentPath]);
 
     return { userAccessToken };
 }
