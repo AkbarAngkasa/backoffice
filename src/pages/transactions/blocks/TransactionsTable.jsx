4d69618d89ee08bc97fa03f8d3999aa614db1a4a
unknown <akbar@emkop.co.id>
2023-05-17 14:34:23 +0800

    modified

--- src/pages/transactions/blocks/TransactionsTable.jsx
+++ src/pages/transactions/blocks/TransactionsTable.jsx
@@ -2,9 +2,9 @@ import React, { useState, useEffect, useRef } from 'react';
 // import { Button as ButtonFlowbite } from 'flowbite-react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import { Button } from 'primereact/button';
+// import { Button } from 'primereact/button';
 import { Tooltip } from 'primereact/tooltip';
-import { InputText } from 'primereact/inputtext';
+// import { InputText } from 'primereact/inputtext';
 import 'primeicons/primeicons.css';
 import "primereact/resources/primereact.min.css";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
@@ -17,6 +17,9 @@ import { useNavigate } from 'react-router';
 import formatRupiah from '../../../methods/formatRupiah';
 import moment from 'moment';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileCsv, faFileExcel, faFilePdf, faSearch } from '@fortawesome/free-solid-svg-icons';
+
 export default function TransactionsTable() {
     PrimeReact.appendTo = 'self';
 
@@ -26,7 +29,7 @@ export default function TransactionsTable() {
 
     // == UI States ==
     const [ fetchingTransactions, setFetchingTransactions ] = useState(false);
-    const [ transactionsArr, setTransactionsArr ] = useState(null);
+    const [ transactionsTable, settransactionsTable ] = useState(null);
 
     // == Datas ==
     const endpoint = process.env.REACT_APP_EMKOP_ENDPOINT_TRANSACTIONS;
@@ -55,7 +58,7 @@ export default function TransactionsTable() {
                     // Stop loading animation
                     setFetchingTransactions(false);
                     // User Logged in.
-                    setTransactionsArr(response.data.rows);
+                    settransactionsTable(response.data.rows);
                 } else if (response.status === 401){
                     // User is Not Logged in.
                     setFetchingTransactions(false);
@@ -71,59 +74,17 @@ export default function TransactionsTable() {
     }, [accessToken, endpoint, navigate]);
     // == End Of Fetch Transactions ==
 
-    // Dummy Data
-    const [products, setProducts] = useState([]);
-    const dt = useRef(null);
-
-    const ProductService = [
-        {
-            number: '1',
-            emkop_transaction_id: 'TRXBRGxxx',
-            emkop_user_id: '<Nomor HP Emkop>',
-            customer_id: '<BIGO ID>',
-            nominal: 25000,
-            transaction_time: '09/05/2023',
-            product_value_1: 84,
-            product_value_2: 'Reserved For Future',
-            billing_id: 'Reserved For Future',
-            reference_id: 'Reserved For Future',
-            status: '<Default: Success, Complete>',
-        },
-        {
-            number: '1',
-            emkop_transaction_id: 'TRXBRGxxx',
-            emkop_user_id: '<Nomor HP Emkop>',
-            customer_id: '<BIGO ID>',
-            nominal: 25000,
-            transaction_time: '09/05/2023',
-            product_value_1: 84,
-            product_value_2: 'Reserved For Future',
-            billing_id: 'Reserved For Future',
-            reference_id: 'Reserved For Future',
-            status: '<Default: Success, Complete>',
-        },
-    ]
-    
-    useEffect(() => {
-        // ProductService.getProductsMini().then((data) => setProducts(data));
-        setProducts(ProductService)
-    }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-    // End Of Dummy Data
-    
     // == File Exports Handlers ==
+    const dt = useRef(null);
     const cols = [
-        { field: 'number', header: '#' },
-        { field: 'emkop_transaction_id', header: 'Emkop Transaction ID' },
-        { field: 'emkop_user_id', header: 'Emkop User ID' },
-        { field: 'customer_id', header: 'Customer ID' },
-        { field: 'nominal', header: 'Nominal' },
-        { field: 'transaction_time', header: 'Transaction Time' },
-        { field: 'product_value_1', header: 'Product Value 1' },
-        { field: 'product_value_2', header: 'Product Value 2' },
-        { field: 'billing_id', header: 'Billing ID' },
-        { field: 'reference_id', header: 'Reference ID' },
-        { field: 'status', header: 'Status' },
+        { field: 'id', header: '#' },
+        { field: 'phone_number', header: 'Emkop User ID' },
+        { field: 'phone_number_destination', header: 'Phone Number Destination' },
+        { field: 'amount', header: 'Amount' },
+        { field: 'diamond', header: 'Diamond' },
+        { field: 'transaction_date', header: 'Transaction Date' },
+        { field: 'status', header: 'status' },
+        { field: 'type', header: 'Type' },
     ];
 
     const exportColumns = cols.map((col) => ({ title: col.header, dataKey: col.field }));
@@ -137,22 +98,22 @@ export default function TransactionsTable() {
             import('jspdf-autotable').then(() => {
                 const doc = new jsPDF.default(0, 0);
 
-                doc.autoTable(exportColumns, products);
-                doc.save('products.pdf');
+                doc.autoTable(exportColumns, transactionsTable);
+                doc.save('transactionsTable.pdf');
             });
         });
     };
 
     const exportExcel = () => {
         import('xlsx').then((xlsx) => {
-            const worksheet = xlsx.utils.json_to_sheet(products);
+            const worksheet = xlsx.utils.json_to_sheet(transactionsTable);
             const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
             const excelBuffer = xlsx.write(workbook, {
                 bookType: 'xlsx',
                 type: 'array'
             });
 
-            saveAsExcelFile(excelBuffer, 'products');
+            saveAsExcelFile(excelBuffer, 'transactionsTable');
         });
     };
 
@@ -195,14 +156,22 @@ export default function TransactionsTable() {
 
     // Components
     const header = (
-        <div className="flex justify-end gap-2">
-            <Button type="button" icon="pi pi-file" rounded onClick={() => exportCSV(false)} data-pr-tooltip="CSV" className='bg-blue-400 hover:bg-red-800' />
-            <Button type="button" icon="pi pi-file-excel" severity="success" rounded onClick={exportExcel} data-pr-tooltip="XLS" />
-            <Button type="button" icon="pi pi-file-pdf" severity="warning" rounded onClick={exportPdf} data-pr-tooltip="PDF" />
-            <span className="p-input-icon-left">
-                <i className="pi pi-search" />
-                <InputText value={globalFilterValue} onChange={onGlobalFilterChange} placeholder="Search" className='w-32' />
-            </span>
+        <div className="flex flex-row justify-between sm:justify-end gap-2 py-2 px-1 w-full overflow-auto h-fit">
+            <button type="button" className="text-white bg-yellow-600 hover:bg-yellow-700 focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-base py-4 px-[1.30rem] block dark:bg-yellow-600 dark:hover:bg-yellow-600 focus:outline-none dark:focus:ring-yellow-700" onClick={() => exportCSV(false)}>
+                <FontAwesomeIcon icon={faFileCsv} />
+            </button>
+            <button type="button" className="text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:ring-green-300 font-medium rounded-full text-base py-4 px-[1.40rem] block dark:bg-green-600 dark:hover:bg-green-600 focus:outline-none dark:focus:ring-green-700" onClick={exportExcel}>
+                <FontAwesomeIcon icon={faFileExcel} />
+            </button>
+            <button type="button" className="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-full text-base py-4 px-[1.30rem] block dark:bg-red-600 dark:hover:bg-red-600 focus:outline-none dark:focus:ring-red-700" onClick={exportPdf}>
+                <FontAwesomeIcon icon={faFilePdf} />
+            </button>
+            <div className="relative">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <FontAwesomeIcon icon={faSearch} className='text-slate-500' />
+                </div>
+                <input type="search" id="default-search" className="block py-4 pl-10 pr-4 w-40 sm:w-full text-sm font-medium border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search.." value={globalFilterValue} onChange={onGlobalFilterChange} required />
+            </div>
         </div>
     );
 
@@ -231,16 +200,22 @@ export default function TransactionsTable() {
                     <span className="sr-only">Loading...</span>
                 </div>
             }
-            {!fetchingTransactions && transactionsArr &&
+            {!fetchingTransactions && transactionsTable &&
                 <div className="card">
                     <Tooltip target=".export-buttons>button" position="bottom" />
-                    <DataTable ref={dt} value={transactionsArr} header={header} tableStyle={{ minWidth: '50rem' }} paginator rows={3} filters={filters} globalFilterFields={['id', 'phone_number', 'phone_number_destination', 'amount', 'diamond', 'transaction_date', 'status', 'type']} emptyMessage="Query Not Found." className='h-screen'>
+                    <DataTable ref={dt} value={transactionsTable} header={header} tableStyle={{ minWidth: '50rem' }} paginator rows={3} filters={filters} globalFilterFields={['id', 'phone_number', 'phone_number_destination', 'amount', 'diamond', 'transaction_date', 'status', 'type']} emptyMessage="Query Not Found." className='h-screen'>
                         <Column field="id" header="#" />
                         <Column field="phone_number" header="Emkop User ID" />
                         <Column field="phone_number_destination" header="Phone Number Destination" />
+                        
                         <Column body={amount} header="Amount" />
+                        <Column field="amount" header="Amount" className='hidden'/>
+                        
                         <Column field="diamond" header="Diamond" />
+                        
                         <Column body={transaction_date} header="Transaction Date" />
+                        <Column field="transaction_date" header="Transaction Date" className='hidden' />
+                        
                         <Column field="status" header="Status" />
                         <Column field="type" header="Type" />
                     </DataTable>
